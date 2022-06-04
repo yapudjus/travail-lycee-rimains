eed9791c775d8bef8f174f21cb10c7797cce5d6a
yapudjus <contact@mail.yapudjusowndomain.fr>
2022-06-04 23:37:03 +0200

    logic and shit all done, will do the slides tmrw

--- history/edison/scroller.js
+++ history/edison/scroller.js
@@ -1,20 +1,13 @@
 function goToID(id) {
     console.log(`going to element ${id}`);
     document.getElementById(id).scrollIntoView({ behavior: "smooth" });
-
 }
 
 function changeClass(object, oldClass, newClass) {
-    // remove:
-    //object.className = object.className.replace( /(?:^|\s)oldClass(?!\S)/g , '' );
-    // replace:
     console.log(`changing class ${oldClass} of object ${object} to ${newClass}`)
     var regExp = new RegExp('(?:^|\\s)' + oldClass + '(?!\\S)', 'g');
     object.className = object.className.replace(regExp, newClass);
-    // add
-    //object.className += " "+newClass;
 }
-
 
 setTimeout(() => {
     (function myLoop(i) {
@@ -23,10 +16,14 @@ setTimeout(() => {
                 console.log(`event_${10-i}`);
                 goToID(id = `event_${10-i}`);
             }
+            if ((9 - i < 8) & (9 - i > -1)) {
+                console.log(`slide_${9-i}`)
+                goToID(`slide_${9-i}`)
+            }
             if (i != 10) {
                 changeClass(object = document.getElementById(`event_${9-i}`), oldClass = "timeline-event-enabled", newClass = "timeline-event");
             }
-            if (1 != 0) {
+            if (i > 1) {
                 changeClass(object = document.getElementById(`event_${10-i}`), oldClass = "timeline-event", newClass = "timeline-event-enabled");
             }
             if (--i) myLoop(i); //  decrement i and call myLoop again if i > 0
